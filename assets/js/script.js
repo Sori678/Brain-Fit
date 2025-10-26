ee96eda567fddd9d16e2336c75ea5ba17ead148d
Sori678 <biv7sorin@gmail.com>
2025-10-26 10:20:58 +0100

    Start create intitGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,13 @@ if (navBarr && navMenu) {
         navMenu.classList.toggle('is-open');
     });
 }
+// game init 
+function initGame(container) {
+    const cells = Array.from(container.querySelectorAll('.vis-memory-box'));
+    const btnStart = container.querySelector('.btn1'); // Start / Next
+    const btnConfirm = container.querySelector('.btn3'); // Confirm
+    const btnReset = container.querySelector('.btn2'); // Reset
+    const statusEl = container.querySelector('.status'); // message UX
+    if (!cells.length || !btnStart || !btnConfirm || !btnReset) return;
+}
+document.querySelectorAll('.con1').forEach(initGame);
